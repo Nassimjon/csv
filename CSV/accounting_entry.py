5186432e3af89bdd1fceb34ff7a123122663f8a6
Nassimjon <nassimulugov@mail.ru>
2026-02-07 15:49:52 +0500

    correctly works

--- CSV/accounting_entry.py
+++ CSV/accounting_entry.py
@@ -29,16 +29,28 @@ acc = cursor.fetchall()
 
 logger.info(f'accounts is {acc}')
 
-def selectAccRest( acc ):
+def selectAccRest(acc):
 
     sel_exp = 'SELECT accRest FROM accounts where account = \'' + acc + '\''
     cursor.execute(sel_exp)
 
     accRest_val = cursor.fetchone()[0]
     logger.info(f'adeline   {accRest_val}')
-    print('adeline ', accRest_val)
+    # print('adeline ', accRest_val)
+    print(f'sel_exp {sel_exp}')
     return accRest_val
 
+def updateAccRest(new_val, acc):
+
+    up_exp = 'UPDATE accounts SET accRest = ' + str(new_val) + ' WHERE account = \'' + acc + '\''
+
+    try:
+        cursor.execute(up_exp)
+        conn.commit()
+        return 1
+    except Exception as e:
+        logger.error(f' errrrrrr {e}')
+        return 0
 
 query_doc = 'SELECT * FROM documents'
 cursor.execute(query_doc)
@@ -46,65 +58,68 @@ docs = cursor.fetchall()
 
 logger.info(f'documents is {docs}')
 
-
 bank_acc = '10101972000100000001'
 try:
     for d in docs:
 
         if d[2] == bank_acc:
+            print('Приход на кассу')
 
-            # print('first acc is bank ')
-            # accRest = 'SELECT accRest FROM accounts where account = \'' + d[3] + '\''
-
-
+            # получаем значение accRest для accCredit
             accRest = selectAccRest(d[3])
-            print('selecr_row  ', accRest)
+            accRestBank = selectAccRest(d[2])
 
-            # cursor.execute(accRest)
-            # accRest_tulpe = cursor.fetchone()
-            # accRest_val = accRest_tulpe[0]
+            newValBank = accRestBank + d[4]
+            updateAccRest(newValBank, d[2])
 
-            new_value = accRest + d[4]
+
+            newVal = accRest + d[4]
+            updateAccRest(newVal, d[3])
+
             print('old val ', accRest)
             print('sum is ', d[4])
-            print('new val ', new_value)
-            update_exp = 'UPDATE accounts SET accRest = ' + str(new_value) + ' WHERE account = \'' + d[3] + '\''
-            # print(update_exp)
-            cursor.execute(update_exp)
-            conn.commit()
-
-            logger.info(accRest)
+            print('new val ', newVal)
 
         elif d[2][-4:] == d[3][-4:]:
-            print('d2 is', d[2][-4:])
+            print('Между своими счетами')
+
+
+            frstAccRest = selectAccRest(d[2])
+            secAccRest = selectAccRest(d[3])
+
+            new_value_frst = frstAccRest - d[4]
+            updateAccRest(new_value_frst, d[2])
+
+            new_val_sec = secAccRest + d[4]
+            updateAccRest(new_val_sec, d[3])
+
             print('sum is', d[4])
-            print('d3 is', d[3][-4:])
-            frstAccRest = 'SELECT accRest FROM accounts where account = \'' + d[2] + '\''
-            secAccRest  = 'SELECT accRest FROM accounts where account = \'' + d[3] + '\''
-            print('secAccRest', secAccRest)
-            cursor.execute(frstAccRest)
+            print('old val firstAcc ', frstAccRest)
+            print('new val firstAcc', new_value_frst)
+            print('\n')
+            print('old val secAcc ', secAccRest)
+            print('new val srcAcc ', new_val_sec)
 
-            accRest_tulpe = cursor.fetchone()
-            frstAccRest_val = accRest_tulpe[0]
-            new_value = frstAccRest_val - d[4]
-            print('old val ', frstAccRest_val)
-            print('new val ', new_value)
-            upFrstAcc = 'UPDATE accounts SET accRest = ' + str(new_value) + ' WHERE account = \'' + d[2] + '\''
-            print('update_first_account  ', upFrstAcc)
-            cursor.execute(upFrstAcc)
 
-            cursor.execute(secAccRest)
-            secAccRest_tulpe = cursor.fetchone()
-            secAccRest_val = secAccRest_tulpe[0]
-            print('secAccRest_val is  ', secAccRest_val)
-            new_val_sec = secAccRest_val + d[4]
-            upSectAcc = 'UPDATE accounts SET accRest = ' + str(new_val_sec) + ' WHERE account = \'' + d[3] + '\''
-            print('second account ', upSectAcc)
-            cursor.execute(upSectAcc)
-            conn.commit()
 
-            print('here is nothing')
         elif d[3] == bank_acc:
+            print('Расход через кассу')
+
+            accRest = selectAccRest(d[2])
+            accRestBank = selectAccRest(d[3])
+
+            newVal = accRest - d[4]
+            updateAccRest(newVal, d[2])
+
+            newValBank = accRestBank - d[4]
+            updateAccRest(newValBank, d[3])
+
+            print('sum is', d[4])
+            print('old val accRest ', accRest)
+            print('new val accRest', newVal)
+            print('\n')
+            print('old val accRestBank ', accRestBank)
+            print('new val accRestBank ', newValBank)
 
             print('stop')
 except Exception as e:
